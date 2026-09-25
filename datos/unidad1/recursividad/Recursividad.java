0daf63ab251d3c386407db645071d95e4ac7dc3d
Nahum-758 <1225100758.ancs@gmail.com>
2026-09-25 13:54:31 -0600

    recursividad saludo nombre

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -12,10 +12,21 @@ public class Recursividad{
 	
 		}
 	}
-
+	
+	public static void cuentaRegresiva(int n) {
+		if(n < 1){ //condicion Base
+			return;
+		
+		} else {
+			System.out.print(n + " ");
+			cuentaRegresiva(n-1);
+		}
+	}
 
 	public static void main(String[] args) {
-		Recursividad(10, "Nahum");
+		//Recursividad(10, "Nahum");
+		cuentaRegresiva(10);
 	}
 
 }
+
